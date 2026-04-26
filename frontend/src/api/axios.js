c5077a4c0a7ab4777489d7dbf6f58b994af93b42
Shayan Ansari <shayanasif519@gmail.com>
2026-04-26 23:48:25 +0500

    Updated backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-ecommerce-orcin-seven.vercel.app/api",
+  baseURL: "https://mern-ecommerce-gamma-ivory.vercel.app/api",
   //   withCredentials: true,
 });
 
